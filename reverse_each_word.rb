a32c73db19d58b096782a0ce412869f20dc9d126
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-18 14:58:59 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   array = string.split
+  reversed_array = []
   array.collect do |word|
-    reversed_array = []
     reversed_array << word.reverse
   end
   return reversed_array.join(" ")
